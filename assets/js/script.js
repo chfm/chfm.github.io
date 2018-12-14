cffa3735f808638ef86aae062fa074f359164f90
Finn James <finn.s.james@gmail.com>
2018-12-13 21:57:25 -0500

    footer update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,50 @@
 // swap out the slogan
 $(function () {
-  var slogans = {
-    0: "Quakers!",
-    1: "we hope your visit to our site was intentional",
-    2: "gluten free",
-    3: "funded by viewers like you, thank you",
-    4: "the best thing since sliced bread",
-    5: "not affiliated with the oats",
-    6: "since 1937",
-    7: "Quakers founded Pennsylvania",
-    8: "Quakers won the 1947 Nobel Peace Prize",
-    9: "part of the Religious Society of Friends",
-    10: "Quakerism was founded by George Fox",
-    11: "locally sourced",
-    12: "assembled in the U.S., designed by George Fox in England",
-    13: "not the same as Amish",
-    14: "not the same as Mormon",
-    15: "not the same as Mennonite, but they used to meet here too",
-    16: "go to Forum!",
-    17: "follow our insta",
-    18: "who said Quakers are humorless?",
-    19: "click my name at the bottom of the page",
-    20: "our old website looked like it was made in the 90s",
-    21: "likely not seen on tv",
-    22: "our favorite is Integrity Spice",
-    23: "there aren't a lot of good ‘quaker’ puns",
-    24: "this site written by hand by yours truly",
-    25: "go Heels!",
-    26: "the official site of the legendary CHFM",
-    27: "we love NPR",
-    28: "from WBEZ and public radio international",
-    29: "the Upper Pod recommends Sufjan Stevens’ Carrie & Lowell",
-    30: "war is not an answer",
-    31: "Quakers!!! Where?",
-    32: "the development name for this website was ‘pink-dandelion’",
-    33: "this Friend speaks my mind",
-    34: "who says Quakers never get anything done?",
-    35: "<3",
-    36: "the favicon is a monocolor version of the official quaker symbol",
-    37: "stop by for tea!",
-    38: "we were pacifists before it was cool",
-    39: "go CFS Fighting Quakers!",
-    40: "don't blame me, I just wrote the website",
-  };
-  var x = Math.floor(Math.random() * (Object.keys(slogans).length));
-  // $("#slogan").html(slogans[x]);
+  var slogans = [
+    "Quakers!!",
+    "we hope your visit to our site was intentional",
+    "gluten free",
+    "funded by viewers like you, thank you",
+    "the best thing since sliced bread",
+    "not affiliated with the oats",
+    "since 1937",
+    "Quakers founded Pennsylvania",
+    "Quakers won the 1947 Nobel Peace Prize",
+    "part of the Religious Society of Friends",
+    "Quakerism was founded by George Fox",
+    "locally sourced",
+    "assembled in the U.S., designed by George Fox in England",
+    "we're not the same as Amish",
+    "we're not the same as Mormon",
+    "we're not the same as Mennonite, but they used to meet here too",
+    "go to Forum!",
+    "follow our insta",
+    "who said Quakers are humorless?",
+    "be sure to stay for snacks",
+    "our old website looked like it was made in the 90s",
+    "likely not seen on tv",
+    "our favorite is Integrity Spice",
+    "there aren't a lot of good ‘quaker’ puns",
+    "this site written by hand by yours truly",
+    "go Heels!",
+    "the official site of the legendary CHFM",
+    "we love NPR",
+    "from WBEZ and public radio international",
+    "the Upper Pod recommends Sufjan Stevens’ Carrie & Lowell",
+    "war is not an answer",
+    "Quakers!!! Where?",
+    "the development name for this website was ‘pink-dandelion’",
+    "this Friend speaks my mind",
+    "who says Quakers never get anything done?",
+    "<3",
+    "the favicon is a monocolor version of the official quaker symbol",
+    "stop by for tea!",
+    "we were pacifists before it was cool",
+    "go CFS Fighting Quakers!",
+    "don't blame me, I just wrote the website"
+  ];
+  var x = Math.floor(Math.random() * (slogans.length));
+  $("#slogan").html(slogans[x]);
 });
 
 // sign up popover
@@ -109,19 +109,6 @@ $(function () {
     $('html,body').animate({
       scrollTop: $card.offset().top - 70
     }, 300);
-  });
-});
-
-// makes the credits appear when you click my name
-$(function () {
-  var state = true;
-  $('#copyright').click(function () {
-    if (state) {
-      $('#creditsReveal').stop().animate({ height: 0 }, 400, 'easeOutExpo');
-    } else {
-      $('#creditsReveal').animate({ height: "100%" }, 400, 'easeOutExpo');
-    }
-    state = !state;
   });
 });
 
